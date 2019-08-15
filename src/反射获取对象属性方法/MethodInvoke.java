b57ad631c9f08a1d75514ae5ebcf5659726cecc3
hjq562004191 <562004191@qq.com>
2019-08-15 21:12:05 +0800

    反射获取对象的属性值和方法的调用

---
+++ src/反射获取对象属性方法/MethodInvoke.java
@@ -0,0 +1,37 @@
+package 反射获取对象属性方法;
+
+import java.lang.reflect.Method;
+
+public class MethodInvoke {
+
+        public static void main(String[] args) throws Exception {
+            Method animalMethod = Animal.class.getDeclaredMethod("print");
+            Method catMethod = Cat.class.getDeclaredMethod("print");
+
+            Animal animal = new Animal();
+            Cat cat = new Cat();
+            animalMethod.invoke(cat);
+            animalMethod.invoke(animal);
+
+            catMethod.invoke(cat);
+            catMethod.invoke(animal);
+        }
+
+    }
+
+class Animal {
+
+    public void print() {
+        System.out.println("Animal.print()");
+    }
+
+}
+
+    class Cat extends Animal {
+
+        @Override
+        public void print() {
+            System.out.println("Cat.print()");
+        }
+
+    }
