a780882381ac5a6e848b6568081f22f5c2d2029c
hjq562004191 <562004191@qq.com>
2019-08-15 10:48:37 +0800

    生产者消费者模式

---
+++ src/生产者消费者模型/PCData.java
@@ -0,0 +1,22 @@
+package 生产者消费者模型;
+
+public final class PCData {
+    private final int intData;
+
+    public PCData(int intData) {
+        this.intData = intData;
+    }
+
+    public PCData(String intData) {
+        this.intData = Integer.valueOf(intData);
+    }
+
+    public int getIntData() {
+        return intData;
+    }
+
+    @Override
+    public String toString() {
+        return "data:"+intData;
+    }
+}
---
+++ src/生产者消费者模型/Producer.java
@@ -0,0 +1,43 @@
+package 生产者消费者模型;
+
+import java.util.Random;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class Producer implements Runnable {
+    private volatile boolean isRunning = true;
+    private BlockingQueue<PCData> queue;                        //内存缓冲区
+    private static AtomicInteger count = new AtomicInteger();   //总数，原子操作
+    private static final int SLEEP_TIME = 1000;
+
+    public Producer(BlockingQueue<PCData> queue) {
+        this.queue = queue;
+    }
+
+    @Override
+    public void run() {
+        PCData data = null;
+        Random r = new Random();
+
+        System.out.println("开始生产 id:" + Thread.currentThread().getId());
+
+        try {
+            while (isRunning) {
+
+                Thread.sleep(r.nextInt(SLEEP_TIME));
+                data = new PCData(count.incrementAndGet());
+                System.out.println(data+"已经放进缓存区");
+                if (!queue.offer(data,2, TimeUnit.SECONDS)){
+                    System.out.println(data+"放入缓存失败");
+                }
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+            Thread.currentThread().interrupt();
+        }
+    }
+    public void stop(){
+        isRunning = false;
+    }
+}
