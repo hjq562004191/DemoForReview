d0acce3d95da965bce4404c95ded7a81b302e63e
hjq562004191 <562004191@qq.com>
2019-10-25 20:23:03 +0800

    File IO

---
+++ src/File/递归遍历目录下的所有文件.java
@@ -0,0 +1,21 @@
+package File;
+
+import java.io.File;
+
+public class 递归遍历目录下的所有文件 {
+    public static void main(String[] args) {
+        File file = new File("D:\\JavaProject");
+        AllFiles(file);
+    }
+
+    public static void AllFiles(File file){
+        if (file.isFile()){
+            System.out.println(file.getName());
+        }else {
+            for (File f:file.listFiles()
+                    ) {
+                AllFiles(f);
+            }
+        }
+    }
+}
